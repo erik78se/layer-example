6c97efc7f1ae6e3b69018c586c9028ffc6564584
Erik Lönroth <erik@impactvi.com>
2018-05-10 21:45:37 +0200

    added part_2

--- reactive/layer_example.py
+++ reactive/layer_example.py
@@ -2,10 +2,13 @@ from charms.reactive import set_flag, when, when_not
 from charmhelpers.core.hookenv import application_version_set, status_set
 from charmhelpers.fetch import get_upstream_version
 import subprocess as sp
+from charmhelpers.core.templating import render
+
 
 @when_not('example.installed')
 def install_example():
     set_flag('example.installed')
+
 
 @when('apt.installed.hello')
 def set_message_hello():
@@ -15,8 +18,31 @@ def set_message_hello():
     # Run hello and get the message
     message = sp.check_output('hello', stderr=sp.STDOUT)
 
-    # Set the active status with the message
-    status_set('active', message )
+    # Set the maintenance status with a message
+    status_set('maintenance', message )
 
     # Signal that we know the version of hello
     set_flag('hello.version.set')
+
+
+@when('database.available')
+def write_text_file(mysql):
+    render(source='text-file.tmpl',
+           target='/root/text-file.txt',
+           owner='root',
+           perms=0o775,
+           context={
+               'my_database': mysql,
+           })
+    status_set('active', 'Ready: File rendered.')
+
+
+@when_not('database.connected')
+def missing_mysql():
+    status_set('blocked', 'Please add relation to MySQL')
+
+
+@when('database.connected')
+@when_not('database.available')
+def waiting_mysql(mysql):
+    status_set('waiting', 'Waiting for MySQL')
