3b14d347bededf2927960dbf7c755052c205258c
Erik Lönroth <erik.lonroth@gmail.com>
2018-05-10 18:31:35 +0200

    Initial commit of layer-example

---
+++ reactive/layer_example.py
@@ -0,0 +1,22 @@
+from charms.reactive import set_flag, when, when_not
+from charmhelpers.core.hookenv import application_version_set, status_set
+from charmhelpers.fetch import get_upstream_version
+import subprocess as sp
+
+@when_not('example.installed')
+def install_example():
+    set_flag('example.installed')
+
+@when('apt.installed.hello')
+def set_message_hello():
+    # Set the upstream version of hello for juju status.
+    application_version_set(get_upstream_version('hello'))
+
+    # Run hello and get the message
+    message = sp.check_output('hello', stderr=sp.STDOUT)
+
+    # Set the active status with the message
+    status_set('active', message )
+
+    # Signal that we know the version of hello
+    set_flag('hello.version.set')
